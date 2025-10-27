d8a472cb2479bbda33a65aad91420e613875693b
Ryan Sepassi <rsepassi@gmail.com>
2025-10-26 21:50:31 -0700

    arm32

---
+++ platform/arm32/platform.mk
@@ -0,0 +1,14 @@
+# ARM32 platform configuration
+
+TARGET = arm-none-eabi
+
+QEMU = qemu-system-arm
+QEMU_MACHINE = virt
+QEMU_CPU = cortex-a15
+QEMU_EXTRA_ARGS =
+
+PLATFORM_CFLAGS = -march=armv7-a
+PLATFORM_LDFLAGS =
+
+# Shared sources from src/ that this platform uses
+PLATFORM_SHARED_SRCS = devicetree.c
